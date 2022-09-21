8d8a8ba54fdfd5c06e68075f5c8059ec11e984cc
Catalin Manaila <cmanaila@comscore.com>
2022-09-21 16:44:45 +0200

    Add multithreading exercise

---
+++ section-4/src/main/java/ro/sdacademy/advanced/_0_multithreading/BankAccount.java
@@ -0,0 +1,28 @@
+package ro.sdacademy.advanced._0_multithreading;
+
+class BankAccount {
+
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+            "balance=" + balance +
+            ", accountNumber='" + accountNumber + '\'' +
+            '}';
+    }
+}
